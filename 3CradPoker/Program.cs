0250a0b19a89fa7dcfba42fa170f80902f4e56bd
kvankadari <krishna.vankadari@ahs.com>
2020-09-28 16:50:07 -0500

    Add project files.

---
+++ 3CradPoker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3CradPoker
+{
+    class Suit
+    {
+        static void Main(string[] args)
+        {
+           var input =  Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 3CradPoker/Suit.cs
@@ -0,0 +1,19 @@
+﻿namespace _3CradPoker
+{
+    public class SuitType : Enumeration
+    {
+        public static readonly SuitType Amex = new SuitType(1, "Amex");
+        public static readonly SuitType Visa = new SuitType(2, "Visa");
+        public static readonly SuitType MasterCard = new SuitType(3, "MasterCard");
+
+        public SuitType(int id, string name)
+            : base(id, name)
+        {
+        }
+
+        // Hearts = "h",
+        // Diamonds = "d",
+        // Spades = "s",
+        // Clubs = "c"
+    }
+}
